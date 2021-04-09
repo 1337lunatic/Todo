8148437f502078b8ad9e2403f2b67178ff134eb1
Jacob Dyreborg Vedel <jaco903a@herningsholm.dk>
2021-04-09 13:05:29 +0200

    SCSS, doing some stuff

---
+++ components/todo-container.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class='item-container'>
+        <ul :key="item.id" v-for="item in todoitems">
+            <li> 
+                <div class="card">
+                    <p>{{ item.name }}</p>
+                    
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+props: ['todoitems']
+
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/css/config';
+
+.item-container{
+    color: $dark-color;
+    
+    ul {
+        margin: 5px;
+
+        li {
+            list-style-type: none;
+        }
+    }
+    p{
+        padding: 0;
+        margin: 0;
+    }
+
+}
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,88 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        Todo
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="container">
+        <div class="todoApp">
+            <div class='title'>
+                Todo items
+            </div>
+			<div>
+				<todo-container :todoitems='todoItems'></todo-container>
+			</div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {}
+import todoContainer from '~/components/todo-container.vue'
+export default {
+  components: { todoContainer },
+	data: () =>{
+		return {
+			todoItems: [
+				{
+					ID: 1,
+					name: "Wash clothes",
+					checked: false
+				},
+				{
+					ID: 2,
+					name: "Buy milk",
+					checked: false
+				},	
+				{
+					ID: 3,
+					name: "Git gut",
+					checked: false
+				},
+				{
+					ID: 4,
+					name: "Sleep",
+					checked: false
+				}
+			]
+		}
+	}
+}
 </script>
 
-<style>
+<style lang='scss'>
+@import '~/assets/css/config';
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  	margin: 0 auto;
+  	min-height: 100vh;
+  	display: flex;
+  	justify-content: center;
+  	text-align: center;
+ 
+	.title {
+  		font-family:
+  		  'Quicksand',
+  		  'Source Sans Pro',
+  		  -apple-system,
+  		  BlinkMacSystemFont,
+  		  'Segoe UI',
+  		  Roboto,
+  		  'Helvetica Neue',
+  		  Arial,
+  		  sans-serif;
+  		display: block;
+  		font-weight: 300;
+  		font-size: 100px;
+  		color: #35495e;
+  		letter-spacing: 1px;
+	}
+	.subtitle {
+		font-weight: 300;
+		font-size: 42px;
+		color: #526488;
+		word-spacing: 5px;
+		padding-bottom: 15px;
+	}
+
+	.links {
+		padding-top: 15px;
+	}
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
 
-.links {
-  padding-top: 15px;
-}
 </style>
